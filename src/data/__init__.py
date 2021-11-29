b69cb83fe15a9ffa87ad3d65b669a197d3a4daab
maximilian.kleissl <maximilian.kleissl@student.hpi.de>
2021-11-29 16:25:06 +0100

    add set5

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -34,13 +34,9 @@ class Data:
 
         self.loader_test = []
         for d in args.data_test:
-            if d in ['Set5', 'Set14', 'B100', 'Urban100']:
-                m = import_module('data.benchmark')
-                testset = getattr(m, 'Benchmark')(args, train=False, name=d)
-            else:
-                module_name = d if d.find('DIV2K-Q') < 0 else 'DIV2KJPEG'
-                m = import_module('data.' + module_name.lower())
-                testset = getattr(m, module_name)(args, train=False, name=d)
+            module_name = d if d.find('DIV2K-Q') < 0 else 'DIV2KJPEG'
+            m = import_module('data.' + module_name.lower())
+            testset = getattr(m, module_name)(args, train=False, name=d)
 
             self.loader_test.append(
                 dataloader.DataLoader(
