41969d8ed79eaf9d3440abef888f63d73c0ece84
Björn Daase <bjoern@daase.net>
2021-11-29 09:09:45 +0100

    More refactoring

--- src/model/rfdn-advanced.py
+++ src/model/rfdn-advanced.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import model.block_advanced as B
 from model import common
 
+
 def make_model(args, parent=False):
     model = RFDNAdvanced(args)
     return model
@@ -13,25 +14,28 @@ class RFDNAdvanced(nn.Module):
         super(RFDNAdvanced, self).__init__()
 
         if args.n_feats != 50:
-            print(f'WARNING: Using non paper num output channels of {args.n_feats} instead of 50.')
-        self.fea_conv = B.conv_layer(args.n_colors, args.n_feats, kernel_size=3)
+            print(
+                f'WARNING: Using non paper num output channels of {args.n_feats} instead of 50.')
+        self.fea_conv = B.conv_layer(
+            args.n_colors, args.n_feats, kernel_size=3)
 
         num_rfdb_blocks = 4
         self.B1 = B.RFDB(in_channels=args.n_feats)
         self.B2 = B.RFDB(in_channels=args.n_feats)
         self.B3 = B.RFDB(in_channels=args.n_feats)
         self.B4 = B.RFDB(in_channels=args.n_feats)
-        self.c = B.conv_block(args.n_feats * num_rfdb_blocks, args.n_feats, kernel_size=1, act_type='lrelu')
+        self.c = B.conv_block(args.n_feats * num_rfdb_blocks,
+                              args.n_feats, kernel_size=1, act_type='lrelu')
 
         self.LR_conv = B.conv_layer(args.n_feats, args.n_feats, kernel_size=3)
 
         upsample_block = B.pixelshuffle_block
-        self.upsampler = upsample_block(args.n_feats, args.n_colors, upscale_factor=args.scale[0])
+        self.upsampler = upsample_block(
+            args.n_feats, args.n_colors, upscale_factor=args.scale[0])
         self.scale_idx = 0
 
         self.sub_mean = common.MeanShift(args.rgb_range)
         self.add_mean = common.MeanShift(args.rgb_range, sign=1)
-
 
     def forward(self, input):
         input = self.sub_mean(input)
