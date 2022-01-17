7eb4358e890a61b91c76fcafa78a08dd16d8788f
Martin Buessemeyer <martin.buessemeyer@student.hpi.de>
2022-01-17 15:47:31 +0100

    Revert "fix exception statement"
    This reverts commit d1e508f87b38f1cdb0c517bc9fdd6083876ae44a.

--- src/utility.py
+++ src/utility.py
@@ -83,7 +83,7 @@ class Checkpoint:
         # try converting to numeric value
         try:
             value = float(value)
-        except:
+        except ValueError as e:
             pass
         if key in self.csv_results_dict.keys():
             self.csv_results_dict[key].append(value)
