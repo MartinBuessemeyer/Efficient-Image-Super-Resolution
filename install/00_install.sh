903a4865ecbc24d0b2a59a9ddcad091307ab88a5
Martin Buessemeyer <martin.buessemeyer@student.hpi.de>
2022-02-21 12:30:19 +0100

    adjust run.sh and container build to latest pytorch

--- install/00_install.sh
+++ install/00_install.sh
@@ -26,6 +26,9 @@ RUN pip install imageio \
     wandb \
     pandas
 
+# Install latest pytorch
+pip3 install torch==1.10.2+cu113 torchvision==0.11.3+cu113 torchaudio==0.10.2+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
+
 # Install development tool for PyCharm for remote debugging
 RUN pip install pydevd-pycharm~=211.7628.24
 
